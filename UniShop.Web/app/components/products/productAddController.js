8e39105cfb7a8d591dbf21d9565e1ff072f4cefe
MUNI <thachmuni1996@gmail.com>
2017-05-10 15:54:02 +0700

    add spa struct

---
+++ UniShop.Web/app/components/products/productAddController.js
@@ -0,0 +1,7 @@
+﻿(function (app) {
+    app.controller('productAddController', productAddController);
+
+    function productAddController() {
+
+    }
+})(angular.module('tedushop.products'));
---
+++ UniShop.Web/app/components/products/productListController.js
@@ -0,0 +1,7 @@
+﻿(function (app) {
+    app.controller('productListController', productListController);
+
+    function productListController() {
+
+    }
+})(angular.module('tedushop.products'));
