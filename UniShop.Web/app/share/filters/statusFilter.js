0ad1a44b9c591548faf507049ae5003e68ed89a7
MUNI <thachmuni1996@gmail.com>
2017-05-17 11:00:38 +0700

    add filter for product category

---
+++ UniShop.Web/app/share/filters/statusFilter.js
@@ -0,0 +1,8 @@
+﻿(function(app) {
+    app.filter('statusFilter',
+        function() {
+            return function(status) {
+                return status? 'Kích hoạt' : 'Khoá';
+            }
+        });
+})(angular.module('unishop.common'));
